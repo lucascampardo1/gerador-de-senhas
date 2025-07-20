84178bbcb5267b914135ae1e167e37da6a2dc39c
Lucas <lucascampardo1@gmail.com>
2025-07-20 20:44:49 -0300

    Gerador de senhas simples com lib e output no console

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using PasswordGenerator;
+using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passwordGenerator = new PasswordGenerator();
+            string password = passwordGenerator.Next();
+            Console.WriteLine($"Generated Password: {password}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gerador-de-senhas
+# Projeto simples de um gerador de senhas em C#
+
+### Foi utilizado a biblioteca 'PasswordGenerator' para gerar as senhas no terminal
